555fccb6210f04d17fe99299d9e856ff66bdae71
Prosper Atu <atu.prosper@gmail.com>
2023-05-12 12:53:55 -0700

    Moved all the models to one file

--- backend/internal/models/models.go
+++ backend/internal/models/models.go
@@ -17,3 +17,30 @@ type User struct {
 	CreatedAt   time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
 	UpdatedAt   time.Time          `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
 }
+
+type Property struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	AgentID     primitive.ObjectID `bson:"agentId"`
+	Title       string             `bson:"title"`
+	Description string             `bson:"description"`
+	Address     Address            `bson:"address"`
+	Price       float64            `bson:"price"`
+	Bedrooms    int                `bson:"bedrooms"`
+	Bathrooms   int                `bson:"bathrooms"`
+	Area        Area               `bson:"area"`
+	CreatedAt   primitive.DateTime `bson:"createdAt,omitempty"`
+	UpdatedAt   primitive.DateTime `bson:"updatedAt,omitempty"`
+}
+
+type Address struct {
+	Street  string `bson:"street"`
+	City    string `bson:"city"`
+	State   string `bson:"state"`
+	Zip     string `bson:"zip"`
+	Country string `bson:"country"`
+}
+
+type Area struct {
+	Value float64 `bson:"value"`
+	Unit  string  `bson:"unit"`
+}
