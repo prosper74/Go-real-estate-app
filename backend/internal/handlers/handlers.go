e157a7f35e532ea0bd1848aeffa3afa09a3bdf3c
Prosper Atu <atu.prosper@gmail.com>
2023-05-12 14:54:23 -0700

    Update handlers

--- backend/internal/handlers/handlers.go
+++ backend/internal/handlers/handlers.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
+	"github.com/justinas/nosurf"
 	"github.com/prosper74/real-estate-app/internal/config"
 	"github.com/prosper74/real-estate-app/internal/db"
 )
@@ -27,8 +30,27 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 
 func (m *Repository) SignUp(w http.ResponseWriter, r *http.Request) {
 	// Logic for handling user registration
+
+	data := make(map[string]interface{})
+	data["CSRFToken"] = nosurf.Token(r)
+
+	out, _ := json.MarshalIndent(data, "", "    ")
+
+	resp := []byte(out)
+	w.Write(resp)
 }
 
 func (m *Repository) PostSignUp(w http.ResponseWriter, r *http.Request) {
 	// Logic for handling user registration
+	first_name := r.Form.Get("first_name")
+	last_name := r.Form.Get("last_name")
+	email := r.Form.Get("email")
+	password := r.Form.Get("password")
+
+	fmt.Println("Username: ", first_name+" "+last_name)
+	fmt.Println("Email: ", email)
+	fmt.Println("Password: ", password)
+
+	resp := []byte(`{"status": "ok"}, {"message": "User created"}`)
+	w.Write(resp)
 }
