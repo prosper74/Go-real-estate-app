45be437d262f53b9bfe5830e2111b4ea9b8ee6d2
Prosper Atu <atu.prosper@gmail.com>
2023-05-15 01:51:40 +0100

    Added navbar and footer

--- frontend/src/components/common/layouts/navbar.tsx
+++ frontend/src/components/common/layouts/navbar.tsx
@@ -1,6 +1,7 @@
-import React, { FC, useState, useEffect } from 'react';
-import Link from 'next/link';
-import { useSelector, RootStateOrAny } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
+// import { useSelector, RootStateOrAny } from 'react-redux';
 // import UserDropdown from './userDropdown';
 // import AuthPortal from '@src/components/auth';
 // import LoginPopupButton from '../buttons/loginPopup';
@@ -12,133 +13,172 @@ import { useSelector, RootStateOrAny } from 'react-redux';
 //   UserIcon,
 // } from '../svgIcons';
 
-const Navbar: FC = () => {
-  const user = useSelector((state: RootStateOrAny) => state.user);
-  const [selectedNav, setSelectedNav] = useState('');
+export default function Navbar() {
+  // const user = useSelector((state: RootStateOrAny) => state.user);
+  const [selectedNav, setSelectedNav] = useState("");
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
-    if (window.location.href.indexOf('/buy') > -1) {
-      setSelectedNav('buy');
-    } else if (window.location.href.indexOf('/rent') > -1) {
-      setSelectedNav('rent');
-    } else if (window.location.href.indexOf('/shortlet') > -1) {
-      setSelectedNav('shortlet');
+    if (window.location.href.indexOf("/buy") > -1) {
+      setSelectedNav("buy");
+    } else if (window.location.href.indexOf("/rent") > -1) {
+      setSelectedNav("rent");
+    } else if (window.location.href.indexOf("/shortlet") > -1) {
+      setSelectedNav("shortlet");
     } else {
-      setSelectedNav('');
+      setSelectedNav("");
     }
   });
 
   return (
-    <div className="container text-center mx-auto">
-      {/* Desktop Menu */}
-      <section className="shadow-lg fixed top-0 inset-x-0 w-full text-gray-700 font-heading font-medium bg-gray-50 bg-opacity-100 z-[10000]">
-        <nav className="flex justify-between px-4 md:px-2 xl:px-32 py-2">
-          <div className="flex w-full items-center">
-            <Link href="/">
-              <a>
-                <img className="h-9" src="/logo.svg" alt="Safe Haven Logo" />
-              </a>
-            </Link>
-            <ul className="hidden md:flex px-4 ml-24 md:ml-10 xl:ml-32">
-              <li className="mr-16">
-                <Link href="/buy">
-                  <a
-                    className={`text-base text-gray-500 hover:text-purple-500 uppercase ${
-                      selectedNav === 'buy' ? 'text-purple-500' : ''
-                    }`}
-                  >
-                    Buy
-                  </a>
-                </Link>
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="https://flowbite.com/" className="flex items-center">
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="h-8 mr-3"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Flowbite
+          </span>
+        </a>
+        <div className="flex items-center md:order-2">
+          <button
+            type="button"
+            className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+            id="user-menu-button"
+            aria-expanded="false"
+            data-dropdown-toggle="user-dropdown"
+            data-dropdown-placement="bottom"
+          >
+            <span className="sr-only">Open user menu</span>
+            <img
+              className="w-8 h-8 rounded-full"
+              src="/docs/images/people/profile-picture-3.jpg"
+              alt="user photo"
+            />
+          </button>
+          {/* Dropdown menu */}
+          <div
+            className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+            id="user-dropdown"
+          >
+            <div className="px-4 py-3">
+              <span className="block text-sm text-gray-900 dark:text-white">
+                Bonnie Green
+              </span>
+              <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
+                name@flowbite.com
+              </span>
+            </div>
+            <ul className="py-2" aria-labelledby="user-menu-button">
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >
+                  Dashboard
+                </a>
               </li>
-              <li className="mr-16">
-                <Link href="/rent">
-                  <a
-                    className={`text-base text-gray-500 hover:text-purple-500 uppercase ${
-                      selectedNav === 'rent' ? 'text-purple-500' : ''
-                    }`}
-                  >
-                    Rent
-                  </a>
-                </Link>
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >
+                  Settings
+                </a>
               </li>
-              <li className="mr-16">
-                <Link href="/shortlet">
-                  <a
-                    className={`text-base text-gray-500 hover:text-purple-500 uppercase ${
-                      selectedNav === 'shortlet' ? 'text-purple-500' : ''
-                    }`}
-                  >
-                    Shortlet
-                  </a>
-                </Link>
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >
+                  Earnings
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >
+                  Sign out
+                </a>
               </li>
             </ul>
-
-            {/* User buttons  */}
-            {user.jwt && user.onboarding ? (
-              <div className="flex items-center ml-auto">
-                <UserDropdown />
-              </div>
-            ) : (
-              <LoginPopupButton
-                isOpen={isOpen}
-                setIsOpen={setIsOpen}
-                buttonText="Login"
-              />
-            )}
           </div>
-        </nav>
-      </section>
-      {/* End of Desktop menu */}
-
-      {/* Mobile menu  */}
-      <nav className="fixed bottom-0 inset-x-0 bg-gray-50 md:hidden flex justify-between text-sm text-gray-700 uppercase font-mono rounded-t-lg shadow-inner z-[10000]">
-        <Link href="/buy">
-          <a className="w-full block py-2 px-3 text-center hover:bg-purple-100 hover:text-purple-500 transition duration-300">
-            <HomeIcon width="25" height="25" fill="#9932cc" />
-            Buy
-          </a>
-        </Link>
-
-        <Link href="/rent">
-          <a className="w-full block py-2 px-3 text-center hover:bg-purple-100 hover:text-purple-500">
-            <RentIcon width="25" height="25" fill="#9932cc" />
-            Rent
-          </a>
-        </Link>
-
-        <Link href="/shortlet">
-          <a className="w-full block py-2 px-3 text-center hover:bg-purple-100 hover:text-purple-500">
-            <ShortletIcon width="25" height="25" fill="#9932cc" />
-            Shortlet
-          </a>
-        </Link>
-
-        {user.jwt && user.onboarding ? (
-          <Link href="/agent/account">
-            <a className="w-full block py-2 px-3 text-center hover:bg-purple-100 hover:text-purple-500">
-              <SettingsIcon width="25" height="25" fill="#9932cc" />
-              Account
-            </a>
-          </Link>
-        ) : (
           <button
-            onClick={() => setIsOpen(true)}
-            className="w-full block py-2 px-3 text-center hover:bg-purple-100 hover:text-purple-500"
+            data-collapse-toggle="mobile-menu-2"
+            type="button"
+            className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="mobile-menu-2"
+            aria-expanded="false"
           >
-            <UserIcon width="25" height="25" fill="#9932cc" />
-            Login
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
           </button>
-        )}
-      </nav>
-      {/* Mobile menu ends here  */}
-
-      {/* Auth Modal Popup */}
-      <AuthPortal isOpen={isOpen} setIsOpen={setIsOpen} />
-    </div>
+        </div>
+        <div
+          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          id="mobile-menu-2"
+        >
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                aria-current="page"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Services
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Pricing
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
-};
-
-export default Navbar;
+}
