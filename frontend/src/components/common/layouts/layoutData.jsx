ca120822000fc39eab80f2cad5ce21f92f6da3bf
Prosper Atu <atu.prosper@gmail.com>
2023-05-16 00:18:16 +0100

    Moved the footer svgs to svgIcons.jsx file

---
+++ frontend/src/components/common/layouts/layoutData.jsx
@@ -0,0 +1,22 @@
+import { FacebookIconFilled, InstagramIcon, TwitterIcon } from "../svgIcons";
+
+export const FooterSocials = [
+  {
+    id: 1,
+    name: "Facebook",
+    icon: <FacebookIconFilled />,
+    url: "#",
+  },
+  {
+    id: 2,
+    name: "Instagram",
+    icon: <InstagramIcon />,
+    url: "#",
+  },
+  {
+    id: 3,
+    name: "Twitter",
+    icon: <TwitterIcon />,
+    url: "#",
+  },
+];
