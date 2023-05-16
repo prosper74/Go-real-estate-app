aa71707b7f95e164282ca8fa45ef7ad8154e7473
Prosper Atu <atu.prosper@gmail.com>
2023-05-16 01:24:32 +0100

    Added main menu and resources menu to layoutdata file

--- frontend/src/components/common/layouts/footer.tsx
+++ frontend/src/components/common/layouts/footer.tsx
@@ -1,4 +1,4 @@
-import { FooterSocials } from "./layoutData";
+import { FooterSocials, MainMenu, ResourcesMenu } from "./layoutData";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -15,57 +15,39 @@ export default function Footer() {
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
               <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                Properties
+              </h2>
+              <ul className="text-gray-600 dark:text-gray-400 font-medium">
+                {MainMenu.map((menu) => (
+                  <li key={menu.id} className="mb-4">
+                    <Link href={menu.url} className="hover:underline">
+                      {menu.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
                 Resources
               </h2>
               <ul className="text-gray-600 dark:text-gray-400 font-medium">
-                <li className="mb-4">
-                  <a href="https://flowbite.com/" className="hover:underline">
-                    Flowbite
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://tailwindcss.com/"
-                    className="hover:underline"
-                  >
-                    Tailwind CSS
-                  </a>
-                </li>
+                {ResourcesMenu.map((menu) => (
+                  <li key={menu.id} className="mb-4">
+                    <Link href={menu.url} className="hover:underline">
+                      {menu.name}
+                    </Link>
+                  </li>
+                ))}
               </ul>
             </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                Follow us
-              </h2>
-              <ul className="text-gray-600 dark:text-gray-400 font-medium">
-                <li className="mb-4">
-                  <a
-                    href="https://github.com/themesberg/flowbite"
-                    className="hover:underline "
-                  >
-                    Github
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://discord.gg/4eeurUVvTy"
-                    className="hover:underline"
-                  >
-                    Discord
-                  </a>
-                </li>
-              </ul>
-            </div>
+
             <div>
               <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
                 Legal
               </h2>
               <ul className="text-gray-600 dark:text-gray-400 font-medium">
-                <li className="mb-4">
-                  <a href="#" className="hover:underline">
-                    Privacy Policy
-                  </a>
-                </li>
                 <li>
                   <a href="#" className="hover:underline">
                     Terms &amp; Conditions
@@ -75,7 +57,9 @@ export default function Footer() {
             </div>
           </div>
         </div>
+
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2023{" "}
--- frontend/src/components/common/layouts/layoutData.jsx
+++ frontend/src/components/common/layouts/layoutData.jsx
@@ -1,5 +1,41 @@
 import { FacebookIconFilled, InstagramIcon, TwitterIcon } from "../svgIcons";
 
+export const MainMenu = [
+  {
+    id: 1,
+    name: "Buy",
+    url: "/buy",
+  },
+  {
+    id: 2,
+    name: "Rent",
+    url: "/rent",
+  },
+  {
+    id: 3,
+    name: "Shortlet",
+    url: "/shortlet",
+  },
+];
+
+export const ResourcesMenu = [
+  {
+    id: 1,
+    name: "How it works",
+    url: "/how-it-works",
+  },
+  {
+    id: 2,
+    name: "About Us",
+    url: "/about",
+  },
+  {
+    id: 3,
+    name: "Contact Us",
+    url: "/contact",
+  },
+];
+
 export const FooterSocials = [
   {
     id: 1,
