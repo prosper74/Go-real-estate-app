41958395e6a32f3f9f1e68a3d41ee5dbb8f278e2
Prosper Atu <atu.prosper@gmail.com>
2023-05-17 10:09:17 +0100

    Added the home banner to home page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,14 +1,8 @@
-import Head from 'next/head';
-import axios from 'axios';
-// import HomeBanner from '@src/components/home/banner';
+import Head from "next/head";
+import HomeBanner from "@src/components/home_components/banner";
 // import HowItWorks from '@src/components/home/howItWorks';
 // import FeaturedProperties from '@src/components/home/featured';
 // import CTASection from '@src/components/home/ctaSection';
-import { singleProperties } from '@src/components/common/interfaces';
-
-interface IProps {
-  properties: singleProperties;
-}
 
 export default function Home() {
   return (
@@ -18,25 +12,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <main className="mt-16">
-        {/* <HomeBanner properties={properties} />
-        <FeaturedProperties properties={properties} />
+        <HomeBanner />
+        {/* <FeaturedProperties properties={properties} />
         <HowItWorks />
         <CTASection /> */}
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident beatae porro corporis nulla voluptate cupiditate, aperiam adipisci repudiandae unde, natus esse nesciunt? Dicta quo consectetur corporis saepe possimus. Cum, excepturi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident beatae porro corporis nulla voluptate cupiditate, aperiam adipisci repudiandae unde, natus esse nesciunt? Dicta quo consectetur corporis saepe possimus. Cum, excepturi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident beatae porro corporis nulla voluptate cupiditate, aperiam adipisci repudiandae unde, natus esse nesciunt? Dicta quo consectetur corporis saepe possimus. Cum, excepturi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident beatae porro corporis nulla voluptate cupiditate, aperiam adipisci repudiandae unde, natus esse nesciunt? Dicta quo consectetur corporis saepe possimus. Cum, excepturi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident beatae porro corporis nulla voluptate cupiditate, aperiam adipisci repudiandae unde, natus esse nesciunt? Dicta quo consectetur corporis saepe possimus. Cum, excepturi.
       </main>
     </>
   );
-};
-
-// export async function getServerSideProps() {
-//   const res = await axios.get(`${process.env.NEXT_PUBLIC_REST_API}/adverts`);
-//   return {
-//     props: {
-//       properties: res.data,
-//     },
-//   };
-// }
+}
