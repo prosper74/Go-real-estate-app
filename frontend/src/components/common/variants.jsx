c5133787740ac5aff70429d110aceac72008dcba
Prosper Atu <atu.prosper@gmail.com>
2023-05-19 09:01:20 +0100

    Finished the CTA section

--- frontend/src/components/common/variants.jsx
+++ frontend/src/components/common/variants.jsx
@@ -8,6 +8,16 @@ export const imageAnimate = {
   },
 };
 
+export const imageAnimateRight = {
+  offscreen: { x: 100, opacity: 0 },
+  onscreen: {
+    x: 0,
+    opacity: 1,
+    rotate: [0, 10, 0],
+    transition: { type: "spring", bounce: 0.4, duration: 1 },
+  },
+};
+
 export const imageAnimateTop = {
   offscreen: { y: -100, opacity: 0 },
   onscreen: {
--- frontend/src/components/home_components/ctaSection.tsx
+++ frontend/src/components/home_components/ctaSection.tsx
@@ -1,18 +1,18 @@
 import { motion } from "framer-motion";
 import {
   textAnimate,
-  imageAnimate,
   imageAnimateTop,
   imageAnimateBottom,
+  imageAnimateRight,
 } from "@src/components/common/variants";
 import { AppleIcon, GooglePlayIcon } from "../common/svgIcons";
 
 export default function CTASection() {
   return (
-    <div className="flex relative items-center overflow-hidden my-16">
-      <div className="container mx-auto xs:px-0 md:px-6 flex relative py-16">
+    <section className="items-center overflow-hidden px-4 mx-auto my-32 sm:!px-10 lg:!px-32">
+      <div className="flex flex-col gap-6 md:flex-row relative">
         <motion.div
-          className="w-full md:w-1/2 flex flex-col relative xl:pl-20"
+          className="flex flex-col relative mb-6"
           initial={"offscreen"}
           whileInView={"onscreen"}
           viewport={{ once: false, amount: 0.5 }}
@@ -23,21 +23,23 @@ export default function CTASection() {
             variants={imageAnimateTop}
           />
           <motion.h1
-            className="font-bebas-neue uppercase text-3xl md:text-5xl xl:text-6xl font-black flex flex-col leading-none dark:text-white text-gray-800"
+            className="font-bebas-neue uppercase text-2xl md:text-3xl xl:text-4xl font-black flex flex-col leading-none dark:text-white text-gray-800"
             variants={textAnimate}
           >
             Our mobile app is coming soon.
           </motion.h1>
+
           <motion.p
-            className="text-sm sm:text-base text-gray-700 dark:text-white"
+            className="mt-3 text-sm sm:text-base text-gray-700 dark:text-white"
             variants={textAnimate}
           >
             Dimension of reality that makes change possible and understandable.
             An indefinite and homogeneous environment in which natural events
             and human existence take place.
           </motion.p>
+
           <motion.div
-            className="flex flex-col sm:flex-row mt-8"
+            className="flex flex-row mt-8"
             variants={imageAnimateBottom}
           >
             <div className="flex mt-3 mr-2 w-48 h-14 bg-black text-white rounded-lg items-center justify-center">
@@ -45,8 +47,8 @@ export default function CTASection() {
                 <GooglePlayIcon />
               </div>
               <div>
-                <div className="text-xs">GET IT ON</div>
-                <div className="text-xl font-semibold font-sans -mt-1">
+                <div className="text-xs">Get it on</div>
+                <div className="text-xs xs:text-lg font-semibold font-sans mt-1">
                   Google Play
                 </div>
               </div>
@@ -57,20 +59,24 @@ export default function CTASection() {
               </div>
               <div>
                 <div className="text-xs">Download on the</div>
-                <div className="text-2xl font-semibold font-sans -mt-1">
+                <div className="text-xs xs:text-lg font-semibold font-sans -mt-1">
                   App Store
                 </div>
               </div>
             </div>
           </motion.div>
         </motion.div>
-        <div className="hidden sm:block sm:w-1/3 lg:w-3/5 relative">
-          <img
+        <div>
+          <motion.img
             src="/assets/images/mobile-app.png"
-            className="max-w-lg m-auto"
+            className="max-w-[300px] sm:max-w-lg"
+            initial={"offscreen"}
+            whileInView={"onscreen"}
+            viewport={{ once: false, amount: 0.3 }}
+            variants={imageAnimateRight}
           />
         </div>
       </div>
-    </div>
+    </section>
   );
 }
