e87c00764c7ffe9196e29bf886ce59ae276cb025
Prosper Atu <atu.prosper@gmail.com>
2023-05-20 08:15:56 +0100

    Removed unneccesary divs

--- frontend/src/components/home_components/howItWorks.tsx
+++ frontend/src/components/home_components/howItWorks.tsx
@@ -33,51 +33,49 @@ export default function HowItWorks() {
           </motion.h2>
         </motion.div>
 
-        <div className="flex flex-wrap md:flex-nowrap items-center justify-center">
-          <div className="w-full mb-12 md:mb-0">
-            <motion.div className="flex flex-wrap">
-              {HowItWorksData.map((data) => (
-                <motion.div
-                  key={data.id}
-                  className="w-full md:w-1/2 xl:w-1/3 px-4"
+        <div className="w-full mb-12 md:mb-0">
+          <motion.div className="flex flex-wrap">
+            {HowItWorksData.map((data) => (
+              <motion.div
+                key={data.id}
+                className="w-full md:w-1/2 xl:w-1/3 px-4"
+              >
+                <div
+                  className={`relative max-w-sm mx-auto mb-8 ${
+                    data.id === 2 && "xl:mt-10"
+                  }`}
                 >
-                  <div
-                    className={`relative max-w-sm mx-auto mb-8 ${
-                      data.id === 2 && "xl:mt-10"
-                    }`}
+                  <motion.div
+                    className="relative p-8 bg-white border border-black border-opacity-10 rounded-3xl z-10"
+                    initial={"offscreen"}
+                    whileInView={"onscreen"}
+                    viewport={{ once: false, amount: 0.5 }}
+                    transition={{ staggerChildren: 0.5 }}
                   >
                     <motion.div
-                      className="relative p-8 bg-white border border-black border-opacity-10 rounded-3xl z-10"
-                      initial={"offscreen"}
-                      whileInView={"onscreen"}
-                      viewport={{ once: false, amount: 0.5 }}
-                      transition={{ staggerChildren: 0.5 }}
+                      className="relative inline-flex items-center justify-center mb-8 w-12 h-12 leading-6 text-white bg-purple-700 rounded-full"
+                      variants={imageAnimate}
                     >
-                      <motion.div
-                        className="relative inline-flex items-center justify-center mb-8 w-12 h-12 leading-6 text-white bg-purple-700 rounded-full"
-                        variants={imageAnimate}
-                      >
-                        <span className="text-2xl font-bold">{data.id}</span>
-                      </motion.div>
-                      <motion.h2
-                        className="mb-4 text-2xl lg:text-3xl leading-tight font-medium font-heading"
-                        variants={textAnimate}
-                      >
-                        {data.heading}
-                      </motion.h2>
-                      <motion.p
-                        className="text-lg text-darkBlueGray-400"
-                        variants={textAnimate}
-                      >
-                        {data.description}
-                      </motion.p>
+                      <span className="text-2xl font-bold">{data.id}</span>
                     </motion.div>
-                    <div className="absolute left-1/2 -bottom-3 transform -translate-x-1/2 w-11/12 h-24 border border-black border-opacity-10 rounded-3xl" />
-                  </div>
-                </motion.div>
-              ))}
-            </motion.div>
-          </div>
+                    <motion.h2
+                      className="mb-4 text-2xl lg:text-3xl leading-tight font-medium font-heading"
+                      variants={textAnimate}
+                    >
+                      {data.heading}
+                    </motion.h2>
+                    <motion.p
+                      className="text-lg text-darkBlueGray-400"
+                      variants={textAnimate}
+                    >
+                      {data.description}
+                    </motion.p>
+                  </motion.div>
+                  <div className="absolute left-1/2 -bottom-3 transform -translate-x-1/2 w-11/12 h-24 border border-black border-opacity-10 rounded-3xl" />
+                </div>
+              </motion.div>
+            ))}
+          </motion.div>
         </div>
         <motion.div
           className="flex w-44 h-1 mx-auto bg-purple-600"
