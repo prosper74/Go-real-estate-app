095607e502620d819dcb247f85058d9a199f626d
Prosper Atu <atu.prosper@gmail.com>
2023-06-04 11:58:42 +0100

    Changed the property card style

--- frontend/src/components/common/properties/propertyCard.tsx
+++ frontend/src/components/common/properties/propertyCard.tsx
@@ -5,19 +5,20 @@ export default function PropertyCard() {
   return (
     <Link
       href="#"
-      className="items-center !bg-white rounded-lg shadow-lg sm:flex cursor-pointer"
+      className="card items-center rounded-lg sm:flex max-h-[19rem] sm:max-h-[16rem]"
     >
+      <div className="background-effect"></div>
       <img
-        className="w-full sm:w-1/2 md:w-[45%] h-[10rem] md:h-[16rem] rounded-lg sm:rounded-none sm:rounded-l-lg object-cover"
+        className="w-full sm:w-1/2 h-1/2 sm:h-full sm:rounded-none sm:rounded-l-lg object-cover"
         src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png"
         alt="Bonnie Avatar"
       />
       <div className="p-4 md:pr-10">
-        <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+        <h3 className="text-xl font-bold tracking-tight">
           Property Name
         </h3>
-        <span className="text-gray-500 dark:text-gray-400">Location</span>
-        <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">
+        <span>Location</span>
+        <p className="mt-3 mb-4 font-light">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita...
         </p>
         <ul className="flex space-x-4 sm:mt-0">
