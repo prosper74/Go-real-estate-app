c2a76f9b6540b97264156476f590dda0df62fa54
Prosper Atu <atu.prosper@gmail.com>
2023-06-04 11:12:12 +0100

    Changed the width of the card image on desktop

--- frontend/src/components/common/properties/propertyCard.tsx
+++ frontend/src/components/common/properties/propertyCard.tsx
@@ -8,7 +8,7 @@ export default function PropertyCard() {
       className="items-center !bg-white rounded-lg shadow-lg sm:flex cursor-pointer"
     >
       <img
-        className="w-full sm:w-1/2 md:w-1/3 h-[10rem] md:h-[16rem] rounded-lg sm:rounded-none sm:rounded-l-lg object-cover"
+        className="w-full sm:w-1/2 md:w-[45%] h-[10rem] md:h-[16rem] rounded-lg sm:rounded-none sm:rounded-l-lg object-cover"
         src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png"
         alt="Bonnie Avatar"
       />
