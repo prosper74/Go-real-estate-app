a258ffd3b953230d630c11ef887944629b211eef
Prosper Atu <atu.prosper@gmail.com>
2023-06-06 08:16:32 +0100

    Added autoplay to slider

--- frontend/src/components/common/properties/propertiesSlider.tsx
+++ frontend/src/components/common/properties/propertiesSlider.tsx
@@ -3,11 +3,11 @@ import PropertyCard from "./propertyCard";
 
 // Import Swiper styles
 import "swiper/css";
-import 'swiper/css/navigation';
+import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 // import required modules
-import { Pagination } from "swiper";
+import { Pagination, Autoplay } from "swiper";
 
 export default function PropertySlider() {
   return (
@@ -16,8 +16,9 @@ export default function PropertySlider() {
         pagination={{
           dynamicBullets: true,
         }}
-        modules={[Pagination]}
+        modules={[Pagination, Autoplay]}
         loop={true}
+        autoplay={{ delay: 5000, disableOnInteraction: true }}
         className="bg-white rounded-lg shadow-lg"
       >
         {[1, 2, 3, 4, 5].map((i) => (
