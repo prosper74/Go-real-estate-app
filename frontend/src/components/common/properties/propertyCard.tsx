bdfdbe37f8049bf917ac491d80749a29e2e1f522
Prosper Atu <atu.prosper@gmail.com>
2023-06-07 09:13:06 +0100

    Removed unsused comments

--- frontend/src/components/common/properties/propertyCard.tsx
+++ frontend/src/components/common/properties/propertyCard.tsx
@@ -27,35 +27,19 @@ export default function PropertyCard({ property }: IProps) {
       <div className="background-effect"></div>
       <img
         className="w-full sm:w-[40%] h-[220px] sm:h-full sm:rounded-none sm:rounded-l-lg object-cover"
+        // @ts-ignore
         src={property.Images[0]}
-        alt="Bonnie Avatar"
+        alt="Property Image"
       />
       <div className="p-4 md:pr-10">
         <h3 className="text-xl md:text-2xl font-bold tracking-tight">
           {property.Title}
         </h3>
 
-        <span className="flex items-center gap-2 mb-4">
+        <span className="flex items-center gap-2 mb-4">          
           <HelpIcon />
           <p>{property.Type} | Period: per year</p>
         </span>
-
-        {/* <ul className="grid grid-cols-2 md:grid-cols-3 gap-3">
-          <li className="flex items-center gap-1">
-            <BedIcon dimensions="w-5 h-5" />
-            <p>{property.Bedroom} Bedrooms</p>
-          </li>
-
-          <li className="flex items-center gap-1">
-            <ShowerIcon dimensions="w-5 h-5" />
-            <p>{property.Bathroom} Bathrooms</p>
-          </li>
-
-          <li className="flex items-center gap-1">
-            <TVIcon dimensions="w-5 h-5" />
-            <p>Kitchen</p>
-          </li>
-        </ul> */}
 
         <PropertyCardMeta property={property} />
 
