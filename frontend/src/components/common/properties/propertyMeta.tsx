10cfc8ed94744024fa314f876475df30332131ce
Prosper Atu <atu.prosper@gmail.com>
2023-06-11 12:21:58 +0100

    Created postgres function to fetch single property

--- frontend/src/components/common/properties/propertyMeta.tsx
+++ frontend/src/components/common/properties/propertyMeta.tsx
@@ -1,12 +1,6 @@
 import React, { FC } from "react";
 import { SingleProperty } from "@src/components/common/interfaces";
-import {
-  BedIcon,
-  HomeIcon,
-  LocationIcon,
-  ShowerIcon,
-  TVIcon,
-} from "../svgIcons";
+import { BedIcon, HomeIcon, ShowerIcon } from "../svgIcons";
 
 interface IProps {
   property: SingleProperty;
@@ -15,18 +9,6 @@ interface IProps {
 export const PropertyMeta: FC<IProps> = ({ property }) => {
   return (
     <>
-      {/* Location */}
-      {/* <ul className="flex items-center my-1 space-x-1 text-lg font-normal leading-4 text-coolGray-500">
-        <li className="flex items-center gap-1">
-          <LocationIcon dimensions="w-5 h-5" />
-          <p>
-            {property.City}, {property.State}
-          </p>
-        </li>
-      </ul> */}
-      {/* End of Location */}
-
-      {/* Meta Description  */}
       <ul className="flex flex-col sm:flex-row items-start sm:items-center my-1 space-x-1 text-base font-normal leading-4 mb-3">
         {property.Bedroom && (
           <li className="flex flex-row items-center gap-1">
@@ -56,7 +38,6 @@ export const PropertyMeta: FC<IProps> = ({ property }) => {
 export const PropertyCardMeta: FC<IProps> = ({ property }) => {
   return (
     <>
-      {/* Meta Description  */}
       <ul className="grid grid-cols-1 lg:grid-cols-2 gap-2">
         {property.Bedroom && (
           <li className="flex items-center gap-1">
