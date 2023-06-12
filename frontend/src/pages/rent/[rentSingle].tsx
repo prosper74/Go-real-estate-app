07fcb76eb46fd343a74a03c505e389551e98dca9
Prosper Atu <atu.prosper@gmail.com>
2023-06-12 10:10:54 +0100

    Single rent page WIP

--- frontend/src/pages/rent/[rentSingle].tsx
+++ frontend/src/pages/rent/[rentSingle].tsx
@@ -1,26 +1,25 @@
-// index.tsx
 import React, { FC } from "react";
 import Head from "next/head";
 import { SingleProperty } from "@src/components/common/interfaces";
 import axios from "axios";
-// import SidebarCard from '@src/components/common/properties/sidebarCard';
-// import { formData } from '@src/components/common/properties/sidebarData';
-// import Breadcrumb from '@src/components/common/layouts/breadcrumb';
-// import SingleProperty from '@src/components/common/properties/singleProperty';
-// import axios from 'axios';
+import { useDispatch } from "react-redux";
+import { setTemplateData } from "@src/store/reducers/templateDataReducer";
 
 interface IProps {
-  properties: {
-    0: SingleProperty;
-  };
+  property: SingleProperty;
 }
 
 const RentSingle: FC<IProps> = ({ data }: any) => {
-  console.log("data:", data);
+  const dispatch = useDispatch();
+  const property = data.property;
+  const templateData = data.templateData;
+  dispatch(setTemplateData({ templateData }));
+  console.log("property:", property);
+
   return (
     <>
       <Head>
-        <title>Rent |{/* {property.title} */}</title>
+        <title>Rent | {property.Title}</title>
         <link rel="icon" href="/favicon.png" />
         <meta content="View all ads of properties that are to be sold" />
       </Head>
@@ -30,16 +29,6 @@ const RentSingle: FC<IProps> = ({ data }: any) => {
           {/* <Breadcrumb category="Rent" property={property.title} /> */}
           <div className="grid grid-cols-1 md:grid-cols-3 gap-0 sm:gap-6 mt-6">
             Rent
-            {/* main properties  */}
-            {/* <div className="col-span-2">
-              <SingleProperty property={property} />
-            </div> */}
-            {/* SideBar  */}
-            {/* <div className="">
-              {formData.map((d) => (
-                <SidebarCard key={d.id} data={d} property={property} />
-              ))}
-            </div> */}
           </div>
         </div>
       </main>
@@ -51,13 +40,6 @@ export default RentSingle;
 
 export async function getServerSideProps(context: any) {
   const { id } = context.query;
-  // const rentSingleId = rentSingle.slice(rentSingle.length - 24);
-  // const res = await axios
-  //   .get(`${process.env.NEXT_PUBLIC_REST_API}/adverts?id=${rentSingleId}`)
-  //   .then((response) => response.data)
-  //   .catch((err) => {
-  //     console.error(err);
-  //   });
 
   const res = await axios.get(`${process.env.NEXT_PUBLIC_REST_API}/rent/${id}`);
 
