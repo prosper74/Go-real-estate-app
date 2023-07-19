1f567c7cb02989bf686c9160a7ac358f98fa18a0
Prosper Atu <atu.prosper@gmail.com>
2023-07-19 09:15:50 +0100

    added inline settings

--- frontend/src/components/common/Buttons/emailVerificationButton.tsx
+++ frontend/src/components/common/Buttons/emailVerificationButton.tsx
@@ -85,7 +85,7 @@ const ResendEmailVerificationButton: FC<IProps> = ({ inline }) => {
       <button
         onClick={handleResendEmailVerification}
         disabled={isDisabled}
-        className="mx-auto py-2 px-6 transition duration-200 text-white disabled:text-gray-300 bg-purple-600 focus:bg-purple-800 focus:shadow-sm focus:ring-4 focus:ring-purple-500 focus:ring-opacity-50 rounded-lg text-lg shadow-sm hover:shadow-md font-semibold text-center flex justify-center items-center"
+        className={`${inline ? 'underline text-sky-700' : 'mx-auto py-2 px-6 transition duration-200 text-white bg-purple-600 focus:bg-purple-800 focus:shadow-sm focus:ring-4 focus:ring-purple-500 focus:ring-opacity-50 rounded-lg text-lg shadow-sm hover:shadow-md font-semibold text-center flex justify-center items-center'} disabled:text-gray-300`}
       >
         {isDisabled ? `Resend Email in 24hrs` : "Resend Email Verification"}
       </button>
