f5f2787940b7e90c1abb86de4ebbcfb9c9b38421
Prosper Atu <atu.prosper@gmail.com>
2023-07-27 12:20:27 +0100

    Changed the styles of user sidebar card

--- frontend/src/components/common/agent/agentSidebar.tsx
+++ frontend/src/components/common/agent/agentSidebar.tsx
@@ -11,11 +11,11 @@ interface IProps {
 const AgentSidebar: FC<IProps> = ({ agent, totalCount }) => {
   return (
     <div className="sticky top-20">
-      <div className="py-2 px-4 rounded-xl shadow-md bg-white">
+      <div className="py-2 px-4 rounded-xl shadow-md bg-white flex flex-col sm:flex-row lg:flex-col items-center justify-evenly gap-1 sm:gap-12 lg:gap-1">
         <img
           src={agent.Image ? agent.Image : "/logoIcon.svg"}
           alt="agent image"
-          className="w-40 h-40 mt-6 rounded-full object-cover"
+          className="w-56 h-56 mt-6 rounded-full object-cover"
         />
         <div className="my-4 text-lg text-gray-700">
           <p className="my-2 capitalize">
