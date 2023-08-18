370ff2dc26146a0a1a1e3e616fd4ed0d1a3914d5
Prosper Atu <atu.prosper@gmail.com>
2023-08-18 10:22:47 +0100

    updated property slider component

--- frontend/src/components/common/properties/propertiesSlider.tsx
+++ frontend/src/components/common/properties/propertiesSlider.tsx
@@ -9,32 +9,38 @@ import "swiper/css/pagination";
 
 // import required modules
 import { Pagination, Autoplay } from "swiper";
+import { useIsMedium, useIsXLarge } from "../hooks/mediaQuery";
 
 interface IProps {
   properties: SingleProperty;
 }
 
 export default function PropertySlider({ properties }: IProps) {
+  const isMedium = useIsMedium();
+  const isXLarge = useIsXLarge();
+
   return (
     <section>
       <Swiper
+        slidesPerView={isXLarge ? 3 : isMedium ? 2 : 1}
         pagination={{
           dynamicBullets: true,
         }}
         modules={[Pagination, Autoplay]}
         loop={true}
         autoplay={{ delay: 5000, disableOnInteraction: true }}
-        className="bg-white rounded-lg shadow-lg"
       >
         {properties.length! >= 1 ? (
           properties.map((d: SingleProperty) => (
-            <SwiperSlide key={d.ID}>
+            <SwiperSlide key={d.ID} className="my-3 m-2">
               <PropertyCard property={d} />
             </SwiperSlide>
           ))
         ) : (
-          <SwiperSlide >
-            <h4 className="h-32 flex items-center justify-center text-2xl">No Item found</h4>
+          <SwiperSlide>
+            <h4 className="h-32 flex items-center justify-center text-2xl">
+              No Item found
+            </h4>
           </SwiperSlide>
         )}
       </Swiper>
