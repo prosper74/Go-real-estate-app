e8823836d290bfb623af5a1b8a272621dd7d33e4
Prosper Atu <atu.prosper@gmail.com>
2023-08-23 17:36:23 +0100

    Added delete fucntion to related properties component

--- frontend/src/components/common/properties/relatedProperties.tsx
+++ frontend/src/components/common/properties/relatedProperties.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useCallback, useEffect, useState } from "react";
 import axios from "axios";
+import crypto from "crypto";
 import { Swiper, SwiperSlide } from "swiper/react";
 import PropertyCard from "./propertyCard";
 import {
@@ -26,6 +27,17 @@ interface IProps {
   propertyId: number;
 }
 
+const generateSHA1 = (data: any) => {
+  const hash = crypto.createHash("sha1");
+  hash.update(data);
+  return hash.digest("hex");
+};
+
+const generateSignature = (publicId: string, apiSecret: string | undefined) => {
+  const timestamp = new Date().getTime();
+  return `public_id=${publicId}&timestamp=${timestamp}${apiSecret}`;
+};
+
 export const RelatedPropertiesSlide: FC<IProps> = ({
   propertyType,
   propertyId,
@@ -33,6 +45,82 @@ export const RelatedPropertiesSlide: FC<IProps> = ({
   const user = useSelector((state: IProps) => state.user);
   const dispatch = useDispatch();
   const [properties, setProperties] = useState<any[]>([]);
+
+  const handleDelete = (propertyID: number, images: string[]) => {
+    axios
+      .get(
+        `${process.env.NEXT_PUBLIC_REST_API}/user/properties?user_id=${user?.userId}&property_id=${propertyID}&jwt=${user?.jwt}`
+      )
+      .then((res) => {
+        if (res.data.error) {
+          dispatch(
+            setSnackbar({
+              status: "error",
+              message: res.data.error,
+              open: true,
+            })
+          );
+        } else {
+          // delete images from cloudinary
+          images.map((image) => {
+            const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_NAME;
+            const timestamp = new Date().getTime();
+            const apiKey = process.env.NEXT_PUBLIC_CLOUDINARY_KEY;
+            const apiSecret = process.env.NEXT_PUBLIC_CLOUDINARY_SECRET;
+            const signature = generateSHA1(generateSignature(image, apiSecret));
+            const url = `https://api.cloudinary.com/v1_1/${cloudName}/image/destroy`;
+
+            axios
+              .post(url, {
+                public_id: image,
+                signature: signature,
+                api_key: apiKey,
+                timestamp: timestamp,
+              })
+              .then(() => {
+                dispatch(
+                  setSnackbar({
+                    status: "success",
+                    message: ` Property deleted`,
+                    open: true,
+                  })
+                );
+              })
+              .catch(() => {
+                dispatch(
+                  setSnackbar({
+                    status: "error",
+                    message: ` Unable to remove property images. Please contact support`,
+                    open: true,
+                  })
+                );
+              });
+          });
+
+          axios
+            .get(
+              `${process.env.NEXT_PUBLIC_REST_API}/property?type=${propertyType}`
+            )
+            .then((res) => {
+              setProperties(res.data.properties);
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        dispatch(
+          setSnackbar({
+            status: "error",
+            message:
+              " There was an error deleting item, please contact support",
+            open: true,
+          })
+        );
+      });
+  };
 
   const handleStatusUpdate = useCallback(
     (propertyID: number, propertyStatus: string) => {
@@ -122,6 +210,7 @@ export const RelatedPropertiesSlide: FC<IProps> = ({
             >
               <PropertyCard
                 property={property}
+                handleDelete={handleDelete}
                 handleStatusUpdate={handleStatusUpdate}
               />
             </SwiperSlide>
