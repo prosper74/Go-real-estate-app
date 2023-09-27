f1e6f947398234ca668809ebd2261c08dc6fa994
Prosper Atu <atu.prosper@gmail.com>
2023-09-27 11:17:44 +0100

    Added some texts if there are no reviews

--- frontend/src/components/common/reviews/index.tsx
+++ frontend/src/components/common/reviews/index.tsx
@@ -58,56 +58,65 @@ export default function Reviews({ propertyID }: IProps) {
     <section className="sm:p-4">
       <Review propertyID={propertyID} setReviews={setReviews} />
 
-      {/* latest reviews  */}
-      {currentReviews?.map((review) => (
-        <div
-          key={review.ID}
-          className="border border-purple-500 rounded-lg p-3 my-3"
-        >
-          <div className="flex gap-2">
-            <CloudinaryImage
-              cloudName={process.env.NEXT_PUBLIC_CLOUDINARY_NAME}
-              publicId={review.User.Image}
-              alt="User image"
-              width="48"
-              height="45"
-              crop="scale"
-              className="rounded-full object-cover"
-            />
-
-            <span>
-              <h3 className="font-bold text-lg text-stone-900">
-                {review.User.FirstName + " " + review.User.LastName}
-              </h3>
-
-              <div className="flex items-center">
-                <span className="flex items-center">
-                  <Rating
-                    number={review.Rating}
-                    dimensions="w-4 h-4"
-                    halfStarDimensions="w-[15px] h-[15px]"
-                  />
-                </span>
-                &nbsp; | &nbsp;
-                <p>
-                  <em>{timeSince(new Date(review.CreatedAt))} ago</em>
-                </p>
-              </div>
-            </span>
-          </div>
-
-          <div className="mt-2">
-            <p>{review.Description}</p>
-          </div>
+      {totalReviews < 1 ? (
+        <div className="text-center my-4">
+          <h3 className="font-semibold text-stone-700">No review for this ad</h3>
+          <p className="text-stone-700">Be the first to review 👌</p>
         </div>
-      ))}
+      ) : (
+        <>
+          {/* latest reviews  */}
+          {currentReviews?.map((review) => (
+            <div
+              key={review.ID}
+              className="border border-purple-500 rounded-lg p-3 my-3"
+            >
+              <div className="flex gap-2">
+                <CloudinaryImage
+                  cloudName={process.env.NEXT_PUBLIC_CLOUDINARY_NAME}
+                  publicId={review.User.Image}
+                  alt="User image"
+                  width="48"
+                  height="45"
+                  crop="scale"
+                  className="rounded-full object-cover"
+                />
 
-      <PaginationWithNavigation
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-        totalReviews={totalReviews}
-        postsPerPage={postsPerPage}
-      />
+                <span>
+                  <h3 className="font-bold text-lg text-stone-900">
+                    {review.User.FirstName + " " + review.User.LastName}
+                  </h3>
+
+                  <div className="flex items-center">
+                    <span className="flex items-center">
+                      <Rating
+                        number={review.Rating}
+                        dimensions="w-4 h-4"
+                        halfStarDimensions="w-[15px] h-[15px]"
+                      />
+                    </span>
+                    &nbsp; | &nbsp;
+                    <p>
+                      <em>{timeSince(new Date(review.CreatedAt))} ago</em>
+                    </p>
+                  </div>
+                </span>
+              </div>
+
+              <div className="mt-2">
+                <p>{review.Description}</p>
+              </div>
+            </div>
+          ))}
+
+          <PaginationWithNavigation
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            totalReviews={totalReviews}
+            postsPerPage={postsPerPage}
+          />
+        </>
+      )}
     </section>
   );
 }
