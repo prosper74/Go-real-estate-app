9328a6bdc4b24c4b4d831a2f08f4d4578dc04744
Prosper Atu <atu.prosper@gmail.com>
2023-11-17 18:50:13 +0100

    writing test for handlers

--- backend/cmd/main_test.go
+++ backend/cmd/main_test.go
@@ -1,8 +1,14 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestRun(t *testing.T) {
+	// Set up the necessary environment variables
+	os.Setenv("POSTGRES_URI", "postgres://fttn:0-Z7ChJAPhNlTP7XdT1OFkRJO")
+
 	_, err := run()
 	if err != nil {
 		t.Error("Failed run()")
